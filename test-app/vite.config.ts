aef4b393c3aabd287f3ffb2df821b783aaea9545
Viswesh934 <sigireddyviswesh@gmail.com>
2025-08-31 22:36:18 +0530

    Vercel.json and based config

--- test-app/vite.config.ts
+++ test-app/vite.config.ts
@@ -6,4 +6,10 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    exclude: ["progress-lite"], // don’t prebundle it with Rollup internals
+  },
+  ssr: {
+    noExternal: ["progress-lite"], // bundle it for SSR so Vercel won’t choke
+  }
 })
